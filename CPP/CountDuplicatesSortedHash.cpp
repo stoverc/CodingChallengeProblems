7e1a5ffc5f43d0ee0b2b51e0967488ac05d642e0
Chris Stover <chaz0014@gmail.com>
2023-05-16 17:24:10 -0500

    Initial commit

---
+++ CPP/CountDuplicatesSortedHash.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+template <typename T>
+T Max(T arr[], int size){
+    T max = INT_MIN;
+
+    for(int i = 0; i < size; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
+
+template <typename T>
+T Min(T arr[], int size){
+    T min = INT_MAX;
+
+    for(int i = 0; i < size; i++){
+        if(arr[i] < min){
+            min = arr[i];
+        }
+    }
+
+    return min;
+}
+template <typename T>
+void CountDuplicatesSortedHash(T arr[], int size){
+    T min = Min(arr, size);
+    T max = Max(arr, size);
+    T hash[max+1];
+
+    for(int i = 0; i < max+1; i++){
+        hash[i] = 0;
+    }
+
+    for(int i = 0; i < size; i++){
+        hash[arr[i]]++;
+    }
+
+    for(int i = 0; i < max+1; i++){
+        if(hash[i] > 1){
+            std::cout << i << " is appearing " << hash[i] << " times." << std::endl;
+        }
+    }
+
+}
+
+int main(){
+    int *init = new int[10]{3,6,8,8,10,12,15,15,15,20};
+    CountDuplicatesSortedHash(init,10);
+}
