2bf248420e039a2b672789023a5a21ef39230947
Chris Stover <chaz0014@gmail.com>
2023-05-16 17:17:49 -0500

    Initial commit

---
+++ CPP/CountDuplicatesSorted.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+template <typename T>
+void CountDuplicatesSorted(T arr[], int size){
+    for(int i = 0; i < size-1; i++){
+        if(arr[i] == arr[i+1]){
+            int j = i+1;
+            while(arr[j] == arr[i]){
+                j++;
+            }
+
+            std::cout << arr[i] << " is appearing " << j-i << " times." << std::endl;
+
+            i = j-1;
+        }
+    }
+}
+
+int main(){
+    int *init = new int[10]{3,6,8,8,10,12,15,15,15,20};
+    CountDuplicatesSorted(init,10);
+}
