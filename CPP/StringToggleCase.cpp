40a2267813cf5303fd980f6120580a6d3ce42109
Chris Stover <chaz0014@gmail.com>
2023-05-17 12:21:09 -0500

    Initial commit

---
+++ CPP/StringToggleCase.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+template <typename T>
+void Print (T arr[], int size){
+    std::cout << "[";
+
+    for(int i = 0; i < size; i++){
+        if(i != size-1){
+            std::cout << arr[i] << ", ";
+        }
+        else{
+            std::cout << arr[i] << "]";
+        }
+    }
+
+    std::cout << std::endl;
+}
+
+void StringToggleCase(char A[]){
+    for(int i = 0; A[i] != '\0'; i++){
+        if(A[i] >= 65 && A[i] <= 90){ //Caps to LC
+            A[i] += 32;
+        }
+        else if(A[i] >= 97 && A[i] <= 122){ //LC to Caps
+            A[i] -= 32;
+        }
+    }
+}
+
+int main (){
+    char A[] = "WeLcOME";
+    Print(A,7);
+    StringToggleCase(A);
+    Print(A,7);
+}
